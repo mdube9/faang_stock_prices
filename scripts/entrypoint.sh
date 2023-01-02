878798d1da5101618cecb74f1fca38c1e74fc3d0
Madhav Dube <madhav.dubey9@gmail.com>
2023-01-01 23:23:58 -0500

    Add files via upload

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,4 +2,5 @@
 airflow db init
 pip install -r /opt/airflow/requirements.txt
 airflow users create --username airflow --firstname Madhav --lastname Dube --password airflow --role Admin --email madhav.dubey9@gmail.com
+airflow connections add postgres_conn --conn-uri 'postgres://user:password@db:5432/my_db'
 airflow webserver
